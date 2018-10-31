5bb0a5f040d562a6f596fc26273651e7ef5dd2f3
Rushi Gandhi <rushi.gandhi@hotmail.com>
2018-10-31 09:16:43 -0400

    Started iOS App

---
+++ VisionLock/VisionLock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionLock
+//
+//  Created by Rushi Gandhi on 2018-10-31.
+//  Copyright © 2018 Rushi Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
